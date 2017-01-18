97ae0dbd11d4c4bc43c39426cfbef965b325e117
Cory Lynch <cjl2625@gmail.com>
2017-01-18 17:48:45 -0500

    finished delicious user line tracking

--- client/js/editor.js
+++ client/js/editor.js
@@ -12,15 +12,18 @@ var mongoId = null;
 var userId = null ;
 var lockTimeout = null;
 var compressTimeout = null;
+var userMarks = {};
 
 Template.EditorPage.onRendered(() => {
+    var doc = $('.CodeMirror')[0].CodeMirror;
+
     try {
       username = Meteor.user()['emails'][0]['address'];
     } catch(e) {} //tfw you're too lazy to be bothered with error handling
 
     var id = FlowRouter.getParam("editID");
 
-    EditUsers.insert({name: username, editor: id, file: fileName, line: [-1, 0]}, function(err, _id) {
+    EditUsers.insert({name: username, editor: id, file: fileName, line: [-1, 0], revive: ''}, function(err, _id) {
         userId = _id;
         Session.set("userId", _id);
         Session.set("editing", true)
@@ -44,6 +47,23 @@ Template.EditorPage.onRendered(() => {
       }
     });
 
+    EditUsers.find({editor:id}).observe({
+       added: function (i) {
+       },
+       changed: function (changes, old) {
+         if(userMarks[changes['_id']]) {
+           userMarks[changes['_id']].clear();
+         }
+         if(changes['line'][0] != -1 && changes['_id'] != userId) {
+            var a = changes['line'][0] //from
+            var b = changes['line'][1]+a-1 //to
+            var mark = doc.markText({line:a, ch:0}, {line:b}, {className: "locked", readOnly: true});
+            userMarks[changes['_id']] = mark;
+         }
+       },
+       removed: function (i) {
+      }
+    });
 });
 
 Template.EditorPage.helpers({
@@ -72,22 +92,36 @@ Template.EditorPage.helpers({
 
     editorEvents() {
        return {
-         "beforeChange": function(doc, change){
+         "change": function(doc, change){
             //Changes.add(change)
             lineObj = [change['from']['line'], change['text'].length];
             change['user'] = userId;
 
-            EditUsers.update({_id:userId}, {$set: {line:lineObj}});
+            var hash = Random.id()
+            var a = lineObj[0] //from
+            var b = lineObj[1]+a-1 //to
+            EditUsers.update({_id:userId}, {$set: {line:lineObj, revive:hash}});
+            //var mark = doc.markText({line:a, ch:0}, {line:b}, {className: "unlocked"});
+            //setTimeout(function() {
+            //  mark.clear();
+            //}, 1000);
+
             clearTimeout(lockTimeout)
             lockTimeout = setTimeout(function() {
                 EditUsers.update({_id:userId}, {$set: {line:[-1,0]}});
-            }, 1500);
+            }, 1000);
 
             clearTimeout(compressTimeout);
-            compressTimeout = setTimeout(function() {
-                l = EditUsers.find({_id:userId}).fetch()[0]['line'][0]
-                EditUsers.update({_id:userId}, {$set: {line:[l,1]}});
-            }, 500);
+            if(lineObj[1] > 1) {
+              compressTimeout = setTimeout(function() {
+                  var oldRange = EditUsers.find({_id:userId}).fetch()[0]['line']
+                  var l = doc.getCursor()['line'];
+                  if(l > oldRange[0] && l < oldRange[0]+oldRange[1]) {
+                    EditUsers.update({_id:userId}, {$set: {line:[l,1]}});
+                  }
+              //    mark.clear();
+              }, 500);
+            }
 
             if(change['origin'] != 'ignore') {
               Changes.update(mongoId, {$set: change});
@@ -100,7 +134,7 @@ Template.EditorPage.helpers({
     },
 
   editorCode() {
-      return "Code to show in editor";
+      return 'for meme in range(6):\n\tbadmeme = meme + 1\n\tprint(badmeme)\n\tprint(meme)\n\tpass';
   }
 });
 
