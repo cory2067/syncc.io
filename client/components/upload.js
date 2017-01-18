3a71edfc268ff205391eca33c5020180c61d97bc
Cory Lynch <cjl2625@gmail.com>
2017-01-18 03:30:14 -0500

    delet syntax error

--- client/components/upload.js
+++ client/components/upload.js
@@ -2,7 +2,7 @@ Template.UploadForm.events({
     'change #files': function(event, template) {
         FS.Utility.eachFile(event, function(file) {
             var myFile = new FS.File(file);
-            myFile.metadata = {owner: Meteor.userId() path: './'};
+            myFile.metadata = {owner: Meteor.userId(), path: './'};
             Documents.insert(myFile, function (err, fileObj) {
                 if (err) {
                     console.log("there was an error", err);
