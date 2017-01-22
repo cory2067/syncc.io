84a7dea8c712ff2b8f8f5fd65a10e1584da22f38
Michelle Tan <mtan123456789@gmail.com>
2017-01-22 07:33:38 -0500

    Can view projects list again

--- client/components/project_list.js
+++ client/components/project_list.js
@@ -1,10 +1,10 @@
 Template.ProjectList.helpers({
     docs: function () {
-        var a = Documents.find({"metadata.owner": Meteor.userId()}).fetch();
+        var a = Documents.find({"userId": Meteor.userId()}).fetch();
         console.log(Meteor.users.find().fetch());
         for(var entry=0; entry<a.length; entry++) {
-          a[entry]['date'] = a[entry].original.updatedAt.toString().substring(0, 15);
-          a[entry]['user'] = Meteor.users.find(a[entry].metadata.owner).fetch()[0].emails[0].address;
+          //a[entry]['date'] = a[entry].original.updatedAt.toString().substring(0, 15);
+          a[entry]['user'] = Meteor.users.find(a[entry].userId).fetch()[0].emails[0].address;
         }
         return a;
     }
