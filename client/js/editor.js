27a1efb7188442f5a030c98d99c1be6e8756ae32
Jessica Tang <jynnie@mit.edu>
2017-01-24 17:04:47 -0500

    Creating modal for new file

--- client/js/editor.js
+++ client/js/editor.js
@@ -312,22 +312,25 @@ Template.EditorPage.helpers({
 
 
 Template.EditorPage.events({
-    'click #newFile': function(event, template) {
-      var nameInput;
-      nameInput = prompt("Name of new file", "helloworld.py");
-      console.log(nameInput);
-      Meteor.call('newFile', [nameInput, Meteor.userId()], function() {
-        Meteor.call("getPath", function(err, path) {
-          var full = path + "/files/" + nameInput;
-          var found = Documents.find({path: full}).fetch()
-          if(found.length > 1) {
-            alert("Please give your file a unique name!");
-          } else if(found.length == 1) {
-            window.location.href = "/" + found[0]['_id'];
-          }
+    'click #newFileBtn': function(event, template) {
+      $(function(){
+        $("#createNewFile").click(function(){
+        var nameInput = $("#fileName").val();
+        console.log(nameInput);
+          Meteor.call('newFile', [nameInput, Meteor.userId()], function() {
+            Meteor.call("getPath", function(err, path) {
+              var full = path + "/files/" + nameInput;
+              var found = Documents.find({path: full}).fetch()
+              if(found.length > 1) {
+                alert("Please give your file a unique name!");
+              } else if(found.length == 1) {
+                window.location.href = "/" + found[0]['_id'];
+              }
+            });
+          });
         });
-    });
-  }
+      });
+    }
 });
 
 Template.EditorPage.onDestroyed(function() {
