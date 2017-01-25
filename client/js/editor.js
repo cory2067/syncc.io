52bab2149bac8fb9d7ec43cc74b431646ac419ac
Jessica Tang <jynnie@mit.edu>
2017-01-25 18:27:58 -0500

    deleted some now unnecessary code

--- client/js/editor.js
+++ client/js/editor.js
@@ -369,26 +369,6 @@ Template.EditorPage.events({
       console.log(file_name +"told to write" + content + " to "+ path);
       Meteor.call('writeFile', [content, path, file_name]);
     },
-
-    /*'click #newFileBtn': function(event, template) {
-      $(function(){
-        $("#createNewFile").click(function(){
-        var nameInput = $("#fileName").val();
-        console.log(nameInput);
-          Meteor.call('newFile', [nameInput, Meteor.userId()], function() {
-            Meteor.call("getPath", function(err, path) {
-              var full = path + "/files/" + nameInput;
-              var found = Documents.find({path: full}).fetch()
-              if(found.length > 1) {
-                alert("Please give your file a unique name!");
-              } else if(found.length == 1) {
-                window.location.href = "/" + found[0]['_id'];
-              }
-            });
-          });
-        });
-      });
-    }*/
 });
 
 Template.EditorPage.onDestroyed(function() {
