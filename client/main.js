cdfd8c7098ddea1e98a3e4e81eb89ecfe321c0ba
Cory Lynch <cjl2625@gmail.com>
2017-01-25 17:51:53 -0500

    improved demo mode

--- client/main.js
+++ client/main.js
@@ -45,15 +45,11 @@ Template.HomePage.events({
     'click #demoEditor': function(event, template) {
         console.log("making new file");
         var nameInput = Random.id(8)+'.py';
-        Meteor.call('newFile', [nameInput, Meteor.userId()], function() {
+        Meteor.call('newFile', [nameInput, ""], function() {
           Meteor.call("getPath", function(err, path) {
             var full = path + "/files/" + nameInput;
             var found = Documents.find({path: full}).fetch()
-            if(found.length > 1) {
-              alert("Please choose a unique file name!");
-            } else if(found.length == 1) {
-              window.location.href = "/" + found[0]['_id'];
-            }
+            window.location.href = "/" + found[0]['_id'];
           });
         });
     }
