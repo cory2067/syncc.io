869e3a4fde45a6f02360d2684ade7ee83b5c9817
Jessica Tang <jynnie@mit.edu>
2017-01-25 11:40:43 -0500

    Saving response kind of works (it kinda shows up twice and idk why)

--- client/js/header.js
+++ client/js/header.js
@@ -2,19 +2,22 @@ import { Documents } from '../../collections/files'
 
 Template.Header.events({
     'click #editor': function(event, template) {
-      var nameInput;
-      nameInput = prompt("Name of new file", "helloworld.py");
-      console.log(nameInput);
-      Meteor.call('newFile', [nameInput, Meteor.userId()], function() {
-        Meteor.call("getPath", function(err, path) {
-          var full = path + "/files/" + nameInput;
-          var found = Documents.find({path: full}).fetch()
-          if(found.length > 1) {
-            alert("Please give your file a unique name!");
-          } else if(found.length == 1) {
-            window.location.href = "/" + found[0]['_id'];
-          }
+      $(function(){
+        $("#createNewFile").click(function(){
+        var nameInput = $("#fileName").val();
+        console.log(nameInput);
+          Meteor.call('newFile', [nameInput, Meteor.userId()], function() {
+            Meteor.call("getPath", function(err, path) {
+              var full = path + "/files/" + nameInput;
+              var found = Documents.find({path: full}).fetch()
+              if(found.length > 1) {
+                alert("Please give your file a unique name!");
+              } else if(found.length == 1) {
+                window.location.href = "/" + found[0]['_id'];
+              }
+            });
+          });
         });
-    });
-  }
+      });
+    }
 });
